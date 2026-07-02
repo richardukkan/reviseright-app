f275c5ade7952c000cf6dbe24a5bae989f3afbc2
richardukkan <97584293+richardukkan@users.noreply.github.com>
2026-07-02 11:57:21 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://cuebkjqbwblmhrwshrsv.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1ZWJranFid2JsbWhyd3NocnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MDk2NTEsImV4cCI6MjA5ODQ4NTY1MX0.Yyh7FBaYdUik9MzGMo8nZUWCsD8HcF4KCreneFjBJXs'
-)
+const URL = 'https://cuebkjqbwblmhrwshrsv.supabase.co'
+const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1ZWJranFid2JsbWhyd3NocnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MDk2NTEsImV4cCI6MjA5ODQ4NTY1MX0.Yyh7FBaYdUik9MzGMo8nZUWCsD8HcF4KCreneFjBJXs'
+
+export const supabase = createClient(URL, KEY)
 
 export const supabaseAdmin = createClient(
-  'https://cuebkjqbwblmhrwshrsv.supabase.co',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY ?? KEY
 )
