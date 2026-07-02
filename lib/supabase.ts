47e561fd914011fc7abd416bd0f244f981692b05
richardukkan <97584293+richardukkan@users.noreply.github.com>
2026-07-02 11:40:16 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://cuebkjqbwblmhrwshrsv.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1ZWJranFid2JsbWhyd3NocnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MDk2NTEsImV4cCI6MjA5ODQ4NTY1MX0.Yyh7FBaYdUik9MzGMo8nZUWCsD8HcF4KCreneFjBJXs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  supabaseUrl,
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 )
